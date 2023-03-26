7b7b3f65cba7825319619f5396dc26b8644cd698
abdulumar1111 <abdulumar1111@gmail.com>
2023-03-26 16:56:37 +0530

    react-native

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+function App() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+
+
+export default App;
